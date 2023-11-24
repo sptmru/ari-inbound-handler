dc3181304429068e551f4c838c89a0f9419d4ae8
Soslan Aldatov <soslanaldatov@gmail.com>
2023-11-25 00:00:13 +0400

    make all methods in InboundNumberService static

--- src/services/InboundNumberService.ts
+++ src/services/InboundNumberService.ts
@@ -1,18 +1,12 @@
-import { Repository } from 'typeorm';
-
 import { dataSource } from '../data-source';
 import { InboundNumber } from '../entities/InboundNumber';
 
 export class InboundNumberService {
-  private repo: Repository<InboundNumber>;
-  constructor() {
-    this.repo = dataSource.getRepository(InboundNumber);
-  }
-  async getInboundNumbers(): Promise<InboundNumber[]> {
-    return await this.repo.find();
+  static async getInboundNumbers(): Promise<InboundNumber[]> {
+    return await dataSource.getRepository(InboundNumber).find();
   }
 
-  async getInboundNumber(phone: string): Promise<InboundNumber | null> {
-    return await this.repo.findOne({ where: { phone } });
+  static async getInboundNumber(phone: string): Promise<InboundNumber | null> {
+    return await dataSource.getRepository(InboundNumber).findOne({ where: { phone } });
   }
 }
