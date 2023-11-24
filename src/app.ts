a1c7d84f1f5b81e7a49040e5d82d3c7030f8fb34
Soslan Aldatov <soslanaldatov@gmail.com>
2023-11-24 23:56:48 +0400

    add simple Stasis app

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,35 @@
+import * as ari from 'ari-client';
 import * as dotenv from 'dotenv';
-import * as winston from 'winston';
+
+import { logger } from './misc/Logger';
+import { Client } from 'ari-client';
 
 const config = dotenv.config().parsed;
 
-const logger = winston.createLogger({
-  level: config?.LOG_LEVEL || 'debug',
-  format: winston.format.combine(winston.format.simple(), winston.format.colorize()),
-  defaultMeta: { service: 'esl' },
-  transports: [new winston.transports.Console()]
-});
+const ariHost = config?.ARI_HOST || 'localhost';
+const ariPort = config?.ARI_PORT || '8088';
+const ariProtocol = config?.ARI_PROTOCOL || 'http';
+const ariUsername = config?.ARI_USERNAME || 'asterisk';
+const ariPassword = config?.ARI_PASSWORD || 'asterisk';
+const ariUrl = `${ariProtocol}://${ariHost}:${ariPort}`;
+
+const callHandler = async (client: Client) => {
+  client.once('StasisStart', async (event, channel) => {
+    logger.debug(`StasisStart on ${channel.name}`);
+    console.dir(event);
+  });
+  client.once('StasisEnd', async (event, channel) => {
+    logger.debug(`StasisEnd on ${channel.name}`);
+    console.dir(event);
+  });
+
+  await client.start(config?.ARI_APP_NAME || 'inbound-app');
+};
+
+ari
+  .connect(ariUrl, ariUsername, ariPassword)
+  .then(callHandler)
+  .catch(err => {
+    logger.error(`Error connecting to ARI: ${err}`);
+    process.exit(1);
+  });
