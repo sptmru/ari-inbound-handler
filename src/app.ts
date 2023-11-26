826ea9be6ca05bec8b51546b62e36da2327db5dd
Soslan Aldatov <soslanaldatov@gmail.com>
2023-11-26 15:42:22 +0400

    add app with number check

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import * as ari from 'ari-client';
 import * as dotenv from 'dotenv';
+import { Client } from 'ari-client';
 
 import { logger } from './misc/Logger';
-import { Client } from 'ari-client';
+import { dataSource } from './data-source.ts';
+import { InboundNumberService } from './services/InboundNumberService';
 
 const config = dotenv.config().parsed;
 
@@ -13,23 +15,43 @@ const ariUsername = config?.ARI_USERNAME || 'asterisk';
 const ariPassword = config?.ARI_PASSWORD || 'asterisk';
 const ariUrl = `${ariProtocol}://${ariHost}:${ariPort}`;
 
-const callHandler = async (client: Client) => {
-  client.once('StasisStart', async (event, channel) => {
-    logger.debug(`StasisStart on ${channel.name}`);
-    console.dir(event);
-  });
-  client.once('StasisEnd', async (event, channel) => {
-    logger.debug(`StasisEnd on ${channel.name}`);
-    console.dir(event);
-  });
+(async () => {
+  try {
+    await dataSource.initialize();
+    logger.debug('Data Source initialized');
+  } catch (err) {
+    logger.error('Error during Data Source initialization', err);
+  }
 
-  await client.start(config?.ARI_APP_NAME || 'inbound-app');
-};
+  const callHandler = async (client: Client) => {
+    client.once('StasisStart', async (event, channel) => {
+      logger.debug(`Inbound call to ${channel.dialplan.exten}`);
+      const inboundNumber = await InboundNumberService.getInboundNumber(channel.dialplan.exten);
+      if (!inboundNumber) {
+        logger.debug(`Number ${channel.dialplan.exten} is not in the database`);
+        try {
+          await channel.continueInDialplan();
+          logger.debug(`Channel ${channel.name} continued in dialplan`);
+        } catch (err) {
+          logger.error(`Error continuing channel ${channel.name} in dialplan`, err);
+        }
+        return;
+      }
+      console.dir(event);
+    });
+    client.once('StasisEnd', async (event, channel) => {
+      logger.debug(`StasisEnd on ${channel.name}`);
+      console.dir(event);
+    });
 
-ari
-  .connect(ariUrl, ariUsername, ariPassword)
-  .then(callHandler)
-  .catch(err => {
-    logger.error(`Error connecting to ARI: ${err}`);
-    process.exit(1);
-  });
+    await client.start(config?.ARI_APP_NAME || 'inbound-app');
+  };
+
+  ari
+    .connect(ariUrl, ariUsername, ariPassword)
+    .then(callHandler)
+    .catch(err => {
+      logger.error(`Error connecting to ARI: ${err}`);
+      process.exit(1);
+    });
+})();
--- src/data-source.ts
+++ src/data-source.ts
@@ -6,16 +6,18 @@ import { InboundNumber } from './entities/InboundNumber';
 
 const config = dotenv.config().parsed;
 
-export const dataSource = new DataSource({
+const dataSource = new DataSource({
   type: 'mysql',
   host: config?.DB_HOST || 'localhost',
   port: parseInt(config?.DB_PORT || '3306'),
   username: config?.DB_USERNAME || 'root',
   password: config?.DB_PASSWORD || '',
   database: config?.DB_NAME || 'inbound_numbers',
-  logging: false,
+  logging: config?.LOG_LEVEL === 'debug',
   synchronize: false,
   entities: [InboundNumber],
   migrations: ['src/migrations/**/*.ts'],
   subscribers: ['src/subscribers/**/*.ts']
 });
+
+export { dataSource };
