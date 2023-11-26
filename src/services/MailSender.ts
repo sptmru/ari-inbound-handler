311457c2bca7092c8b0d9d4e5c13e369814a9496
Soslan Aldatov <soslanaldatov@gmail.com>
2023-11-26 16:14:31 +0400

    MailSender ready-to-use

--- src/services/MailSender.ts
+++ src/services/MailSender.ts
@@ -8,26 +8,31 @@ const config = dotenv.config().parsed;
 export class MailSender {
   static createTransport() {
     return nodemailer.createTransport({
-      host: 'smtp.example.com',
-      port: 587,
-      secure: false, // upgrade later with STARTTLS
+      host: config?.SMTP_HOST || 'smtp.example.com',
+      port: Number(config?.SMTP_PORT) || 587,
+      secure: Boolean(config?.SMTP_SECURE) || false, // upgrade later with STARTTLS
       auth: {
-        user: 'username',
-        pass: 'password'
+        user: config?.SMTP_USERNAME || 'username',
+        pass: config?.SMTP_PASSWORD || 'password'
+      },
+      tls: {
+        ciphers: 'SSLv3'
       }
     });
   }
 
-  static createMailOptions(): Mail.Options {
+  static createMailOptions(text: string, attachments?: string[]): Mail.Options {
     return {
       from: config?.MAIL_FROM || '',
-      subject: config?.MAIL_SUBJECT || ''
+      subject: config?.MAIL_SUBJECT || '',
+      text,
+      attachments: attachments?.map(attachment => ({ filename: attachment }))
     };
   }
 
-  static async sendMail(to: string) {
+  static async sendMail(to: string, text: string, attachments?: string[]): Promise<boolean> {
     const transporter = MailSender.createTransport();
-    const mail = MailSender.createMailOptions();
+    const mail = MailSender.createMailOptions(text, attachments);
     mail.to = to;
 
     try {
