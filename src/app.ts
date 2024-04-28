43e273871d4be09a6b071bdb27a63da07edb2130
Soslan Aldatov <soslanaldatov@gmail.com>
2024-04-28 21:42:57 +0400

    add FIX ME comment to test git hook

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,8 @@ const ariUsername = config.ari.username;
 const ariPassword = config.ari.password;
 const ariUrl = config.ari.url;
 
+console.log('testtesttest'); // FIXME: test git
+
 (async () => {
   try {
     await dataSource.initialize();
