a0e8ff0d82f11a74be28bdbec3bf5ecc2a351438
Soslan Aldatov <soslanaldatov@gmail.com>
2024-04-28 17:41:54 +0400

    fix log filename

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,7 +6,7 @@ export const config = {
   log: {
     level: parsedConfig?.LOG_LEVEL != null ? parsedConfig.LOG_LEVEL : 'debug',
     directory: parsedConfig?.LOG_DIRECTORY != null ? parsedConfig.LOG_DIRECTORY : './logs',
-    file: parsedConfig?.LOG_LEVEL != null ? parsedConfig.LOG_LEVEL : 'debug.log',
+    file: parsedConfig?.LOG_LEVEL != null ? `${parsedConfig.LOG_LEVEL}.log` : 'debug.log',
     logToFile: parsedConfig?.LOG_TO_FILE != null ? parsedConfig.LOG_TO_FILE.toLowerCase() === 'true' : false
   },
   db: {
