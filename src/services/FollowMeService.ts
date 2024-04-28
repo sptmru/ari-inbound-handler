eaf8439539c761f455aa2bb9e19f63750cae8810
Soslan Aldatov <soslanaldatov@gmail.com>
2024-04-28 21:45:50 +0400

    remove FIX ME comment to test git hook

--- src/services/FollowMeService.ts
+++ src/services/FollowMeService.ts
@@ -1,7 +1,6 @@
 import { FollowMe } from '../entities/FollowMe';
 import { dataSource } from '../data-source';
 
-// FIXME: test git
 export class FollowMeService {
   static async getFollowMeData(extension: string): Promise<FollowMe | null> {
     return await dataSource.getRepository(FollowMe).findOne({ where: { extension } });
