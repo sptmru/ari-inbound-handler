7267c2d5d027fb8554b60284b06a661198efb717
Soslan Aldatov <soslanaldatov@gmail.com>
2024-04-28 17:42:22 +0400

    use different log formats for console logging and logging to file

--- src/misc/Logger.ts
+++ src/misc/Logger.ts
@@ -1,22 +1,26 @@
 import * as winston from 'winston';
 import { config } from '../config/config';
 
-const timestampFormat = winston.format.combine(
+const consoleLogFormat = winston.format.combine(
   winston.format.colorize(),
+  winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+  winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+);
+
+const fileLogFormat = winston.format.combine(
   winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
   winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
 );
 
 const logger = winston.createLogger({
   level: config.log.level,
-  format: timestampFormat,
   defaultMeta: {},
   transports: config.log.logToFile
     ? [
-        new winston.transports.Console(),
-        new winston.transports.File({ filename: `${config.log.directory}/${config.log.file}` })
+        new winston.transports.Console({ format: consoleLogFormat }),
+        new winston.transports.File({ filename: `${config.log.directory}/${config.log.file}`, format: fileLogFormat })
       ]
-    : [new winston.transports.Console()]
+    : [new winston.transports.Console({ format: consoleLogFormat })]
 });
 
 export { logger };
