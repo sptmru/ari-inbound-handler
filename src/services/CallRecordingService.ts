f81b1afe917eb432b717159bb93d81cdbc7460da
Soslan Aldatov <soslanaldatov@gmail.com>
2024-04-29 02:57:53 +0400

    handle RecordingFinished event correctly in CallRecordingService

--- src/services/CallRecordingService.ts
+++ src/services/CallRecordingService.ts
@@ -3,19 +3,48 @@ import { dataSource } from '../data-source';
 import { CallRecording } from '../entities/CallRecording';
 import { AriData } from '../types/AriData';
 import { logger } from '../misc/Logger';
+import { config } from '../config/config';
+import { WavService } from './WavService';
+import { CallRecordingOptions } from '../types/CallRecordingOptions';
 
 export class CallRecordingService {
+  static async recordingFinishedEventHandler(ariData: AriData, recordingOptions: CallRecordingOptions): Promise<void> {
+    const { channel, liveRecording } = ariData;
+    logger.debug(`Recording finished on channel ${channel.id}`);
+
+    if (!liveRecording) {
+      logger.error(`No live recording found for channel ${channel.id}`);
+      return;
+    }
+
+    const recording = await liveRecording.copyStored({
+      destinationRecordingName: `${config.callRecording.directory}/${recordingOptions.name}`
+    });
+    const recordingFilePath = `${config.callRecording.baseDirectory}/${recording.name}.${recordingOptions.format}`;
+    const duration = await WavService.getWavFileDuration(recordingFilePath);
+
+    await this.addCallRecordingToDb({
+      callerid_name: channel.caller.name,
+      callerid_num: channel.caller.number,
+      duration,
+      path_to_file: recordingFilePath,
+      court_id: 0,
+      rdnis: ''
+    });
+  }
+
   static async createRecordingChannel(ariData: AriData): Promise<LiveRecording> {
     const { client, channel, appName } = ariData;
-    const recordingOptions = {
+    logger.debug(`Starting call recording on channel ${channel.id}`);
+
+    const recordingOptions: CallRecordingOptions = {
       channelId: channel.id,
-      name: `${Date.now()}`, // TODO: add a unique identifier (for example, phone number)
+      name: `${channel.caller.number}-${Date.now()}.wav`,
       format: 'wav',
       maxDurationSeconds: 0,
       maxSilenceSeconds: 0,
       ifExists: 'overwrite'
     };
-
     const snoopChannel = await client.channels.snoopChannelWithId({
       channelId: channel.id,
       snoopId: `${channel.id}-snoop`,
@@ -23,14 +52,18 @@ export class CallRecordingService {
       spy: 'both'
     });
 
-    const liveRecording = client.LiveRecording();
-    await snoopChannel.record(recordingOptions, liveRecording);
-    logger.debug(`Recording started on channel ${channel.id}`);
+    const liveRecording = await snoopChannel.record(recordingOptions, client.LiveRecording());
+    const ariDataWithLiveRecording = { ...ariData, liveRecording };
+
+    liveRecording.once('RecordingFinished', async () => {
+      await CallRecordingService.recordingFinishedEventHandler(ariDataWithLiveRecording, recordingOptions);
+    });
 
     return liveRecording;
   }
 
   static async addCallRecordingToDb(callRecording: CallRecording): Promise<CallRecording | null> {
+    logger.debug(`Adding call recording to database: ${callRecording.path_to_file}`);
     return await dataSource.getRepository(CallRecording).save(callRecording);
   }
 }
