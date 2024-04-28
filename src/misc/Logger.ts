1b0b3f493f347c04046879afda22117e8b474e7e
Soslan Aldatov <soslanaldatov@gmail.com>
2024-04-28 17:28:49 +0400

    update logging format

--- src/misc/Logger.ts
+++ src/misc/Logger.ts
@@ -1,9 +1,15 @@
 import * as winston from 'winston';
 import { config } from '../config/config';
 
+const timestampFormat = winston.format.combine(
+  winston.format.colorize(),
+  winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+  winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+);
+
 const logger = winston.createLogger({
   level: config.log.level,
-  format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
+  format: timestampFormat,
   defaultMeta: {},
   transports: [new winston.transports.Console()]
 });
