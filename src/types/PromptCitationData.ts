94d6dac2b25a565e7fb52c1ec12a80c8c0376f69
Soslan Aldatov <soslanaldatov@gmail.com>
2024-06-23 20:45:44 +0400

    add prompt citation data type

---
+++ src/types/PromptCitationData.ts
@@ -0,0 +1,8 @@
+export type PromptCitationData = {
+  extension?: string;
+  courtId: string;
+  citationNumber: string;
+  callerIdName: string;
+  callerIdNumber: string;
+  dialedPhoneNumber: string;
+};
